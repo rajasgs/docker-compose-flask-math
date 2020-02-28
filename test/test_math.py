acb50e865ba320771e62e8afdc20a2d79dee5bad
Raja Raman <raja.raman@kwikee.com>
2020-02-28 15:07:23 -0500

    Math test cases added

--- test/test_math.py
+++ test/test_math.py
@@ -1,12 +1,17 @@
 import pytest
 
-def add(a, b):
+import math_util
 
-    return a + b
-
-def test_math():
+def test_math_add():
     
     a = 1
     b = 2
 
-    assert add(a, b) == 3
+    assert math_util.add(a, b) == 3
+
+def test_math_subtract():
+    
+    a = 10
+    b = 2
+
+    assert math_util.subtract(a, b) == 8
