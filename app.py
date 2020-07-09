2007b360704eb3bb3c7e46472d4838537074a100
Raja Raman <raja.raman@kwikee.com>
2020-07-09 10:05:59 -0400

    Port changed

--- app.py
+++ app.py
@@ -55,4 +55,4 @@ def reverse_string_api():
     return jsonify(result) 
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=8071, debug=True)
+    app.run( host='0.0.0.0', port=8072, debug=True)
